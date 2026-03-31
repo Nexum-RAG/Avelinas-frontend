7951d4173666ac31580b313eed3a7dd8a10541d7
Nexum-RAG <agencia.ia.arg@gmail.com>
2026-03-30 23:44:53 -0300

    server.js

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const app = express();
 app.use(express.static(path.join(__dirname)));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  // Si la ruta tiene extensión (ej: .html, .js, .css), dejar que el static lo maneje
+  if (path.extname(req.path)) {
+    res.status(404).send('Not found');
+  } else {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  }
 });
 
 const PORT = process.env.PORT || 3000;
